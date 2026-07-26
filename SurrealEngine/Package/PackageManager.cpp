f4e17c7c2b2e33d4f75f528d370050fff4beadec
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-07-26 13:57:10 +0200

    GC is not in the engine package

--- SurrealEngine/Package/PackageManager.cpp
+++ SurrealEngine/Package/PackageManager.cpp
@@ -1057,7 +1057,7 @@ void PackageManager::RegisterNativeClasses()
 		RegisterNativeClass<UDeusExLevelInfo>(deusExPackage, "DeusExLevelInfo", "Info");
 		RegisterNativeClass<UDXTextParser>(deusExTextPackage, "DeusExTextParser", "Object");
 		RegisterNativeClass<UDXSaveInfo>(deusExPackage, "DeusExSaveInfo", "Object");
-		RegisterNativeClass<UGC>(enginePackage, "GC", "ExtensionObject");
+		RegisterNativeClass<UGC>(extensionPackage, "GC", "ExtensionObject");
 		RegisterNativeClass<UWindow>(extensionPackage, "Window", "ExtensionObject");
 		RegisterNativeClass<UViewportWindow>(extensionPackage, "ViewportWindow", "Window");
 		RegisterNativeClass<UToggleWindow>(extensionPackage, "ToggleWindow", "ButtonWindow");
