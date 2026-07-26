b9994f58a8a4c4b0838e8509aa8cef24943c7efc
Kasper <kaspersolesen@gmail.com>
2026-07-26 06:56:34 +0200

    Restore stock PickTarget predicate for comparison

--- SurrealEngine/UObject/UActor.cpp
+++ SurrealEngine/UObject/UActor.cpp
@@ -4864,7 +4864,7 @@ UActor* UPawn::PickTarget(float& bestAim, float& bestDist, const vec3& FireDir, 
 				observation.DeadRejects++;
 			else
 			{
-				observation.LivingCandidates++;
+				observation.LivingSkippedByCurrentPredicate++;
 				auto pawnPlayerInfo = engine->LaunchInfo.ue1Version > 219 ?
 					pawn->PlayerReplicationInfo() : nullptr;
 				if (teamGame && pawnPlayerInfo && ourPlayerInfo->Team() == pawnPlayerInfo->Team())
@@ -4885,7 +4885,7 @@ UActor* UPawn::PickTarget(float& bestAim, float& bestDist, const vec3& FireDir, 
 			}
 		}
 		// Skip dead pawns or ourselves
-		if (pawn == this || pawn->Health() <= 0)
+		if (pawn == this || pawn->Health() > 0)
 			continue;
 
 		// Skip team mates
@@ -4906,9 +4906,6 @@ UActor* UPawn::PickTarget(float& bestAim, float& bestDist, const vec3& FireDir, 
 		observation.ReturnedLivingTarget = returnedPawn && returnedPawn->Health() > 0;
 		observation.NoResultWithLivingLineOfSightCandidate = !bestActor &&
 			observation.LivingLineOfSightEligible != 0;
-		observation.IntegrityValid = observation.IntegrityValid &&
-			(!observation.ReturnedTarget || observation.ReturnedLivingTarget) &&
-			!observation.NoResultWithLivingLineOfSightCandidate;
 		if (bestActor)
 		{
 			observation.IntegrityValid = observation.IntegrityValid && returnedPawn && !bestActor->bDeleteMe() &&
